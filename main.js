86cdb8b3cea9beece0439e9570ab64dc55d28b9c
Tim <chibuiketimokonkwo@gmail.com>
2022-11-02 12:47:07 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+import React from "react"
+import App from "./src/App"
+import "./src/style.css"
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
